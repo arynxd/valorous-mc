100b8e4d58eafd645b59d6d29d73cc78e29c8895
Aryn Folkard <aryn.folkard@null.net>
2021-02-09 22:36:11 +0000

    added transcripts

--- src/main/java/me/arynxd/valorous/commands/CloseCommand.java
+++ src/main/java/me/arynxd/valorous/commands/CloseCommand.java
@@ -1,10 +1,18 @@
 package me.arynxd.valorous.commands;
 
+import java.awt.*;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 import me.arynxd.valorous.entities.command.Command;
 import me.arynxd.valorous.entities.command.CommandEvent;
+import me.arynxd.valorous.entities.config.ConfigOption;
 import me.arynxd.valorous.entities.database.Ticket;
 import me.arynxd.valorous.util.EmbedUtil;
+import net.dv8tion.jda.api.entities.MessageChannel;
 
 @SuppressWarnings("unused")
 public class CloseCommand extends Command
@@ -27,7 +35,34 @@ public class CloseCommand extends Command
 		}
 
 		ticket.close(event.getValorous());
-		event.getTextChannel().delete().queueAfter(10, TimeUnit.SECONDS, null, error -> {});
-		EmbedUtil.sendSuccess(event.getChannel(), "Ticket closed!");
+
+		EmbedUtil.sendSuccess(event.getChannel(), "Ticket closed! Generating transcripts.");
+
+		event.getTextChannel().getIterableHistory().takeUntilAsync(Objects::isNull).thenAccept(messages ->
+		{
+			event.getTextChannel().delete().queueAfter(10, TimeUnit.SECONDS, null, error -> {});
+			StringBuilder transcript = new StringBuilder();
+			messages.stream()
+					.filter(message -> !message.getAuthor().isBot())
+					.forEach(message -> transcript
+							.append("{ ")
+							.append(message.getAuthor().getAsTag())
+							.append(" / ")
+							.append(message.getAuthor().getId())
+							.append(" }")
+							.append("  ->  ")
+							.append(message.getContentDisplay())
+							.append("\n"));
+
+			InputStream file = new ByteArrayInputStream(transcript.toString().getBytes(StandardCharsets.UTF_8));
+			MessageChannel errorChannel = event.getValorous().getJDA().getTextChannelById(event.getValorous().getConfigHandler().getString(ConfigOption.ERRORCHANNEL));
+			if(errorChannel != null)
+			{
+				errorChannel
+						.sendMessage("Transcripts for ticket " + ticket.getChannelId() + " started by user " + "<@" + ticket.getUserId() + ">")
+						.allowedMentions(Collections.emptyList())
+						.addFile(file, "transcript--" + ticket.getUserId() + ".txt").queue();
+			}
+		});
 	}
 }
